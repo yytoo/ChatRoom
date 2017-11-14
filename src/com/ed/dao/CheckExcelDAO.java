37282e87e2a7ebb85d9a3203cd55d32b4a90522a
yytoo <yaot0102@163.com>
2017-11-14 16:46:49 +0800

    ver1.2

--- src/com/ed/dao/CheckExcelDAO.java
+++ src/com/ed/dao/CheckExcelDAO.java
@@ -1,14 +1,20 @@
 package com.ed.dao;
 
-import java.util.Map;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.hibernate.Session;
 
 import com.cr.util.HibernateSessionFactory;
 
 public class CheckExcelDAO {
-	public Map<String, String> getDsProduct(){
-		Map;
+	public Set<String> getDsProduct(){
+		Set<String> ds_product = new HashSet<String>();
 		Session session = HibernateSessionFactory.getSession();
+		String sql="select prod_name from ds_product";
+		List
+		
+		return ds_product;
 	}
 }
